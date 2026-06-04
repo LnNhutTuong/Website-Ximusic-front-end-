4d87d4eb57ae1b09e5dde19f1ef9c88dedeb0daa
tuong <you@example.com>
2026-06-04 22:45:53 +0700

    implement login validation and handle login API call

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,124 +1,144 @@
 import { useState } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
+import { handleLogin } from "../../services/userService";
 
 const Login = (props) => {
+  const [valueLogin, setValueLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [isValidInput, setIsValidInput] = useState({
+    isValidValueLogin: true,
+    isValidPassword: true,
+  });
 
-    const [valueLogin, setValueLogin] = useState("");
-    const [password, setPassword] = useState("");
-    const [isValidInput, setIsValidInput] = useState({
-        isValidValueLogin: true,
-        isValidPassword: true
-    });
+  const isValid = () => {
+    let check = true;
+    let error = "";
+    const validation = {
+      isValidValueLogin: true,
+      isValidPassword: true,
+    };
 
-    const isValid = () => {
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let phoneRegex = /^\d{10}$/;
 
-        let check = true;
-        let error = "";
-        const validation = {
-            isValidValueLogin: true,
-            isValidPassword: true
-        };
-        
-        let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        let phoneRegex = /^\d{10}$/;
-
-        if(!valueLogin || !password){
-            validation.isValidPassword = false;
-            validation.isValidValueLogin = false;
-            check = false;
-            error = "Please fill in all the fields";
-        }
-
-        if(!valueLogin || valueLogin.match(emailRegex || phoneRegex)){
-            validation.isValidValueLogin = false;
-            check = false;
-            error = "Your Email or Phone number is invalid";
-        }
-
-        if(!password || password.length < 6){
-            validation.isValidPassword = false;
-            check = false;
-            error = "Your Password is invalid";
-        }
-
-        setIsValidInput(validation);
-
-        if(!check && error){
-            toast.error(error);
-            return false;
-        }
-
-        return true;
+    if (!valueLogin || !password) {
+      validation.isValidPassword = false;
+      validation.isValidValueLogin = false;
+      check = false;
+      error = "Please fill in all the fields";
     }
 
-    const handleSubmit = () => {
-        if(!isValid){
-            return;
-        }
-
-        let check = isVali();
-        let data = 
-        if(check){
-            console.log(">>>>>check data: ", data);
-            alert("Login Successfully");
-        }
-
-
+    if (!valueLogin || valueLogin.match(emailRegex || phoneRegex)) {
+      validation.isValidValueLogin = false;
+      check = false;
+      error = "Your Email or Phone number is invalid";
     }
+
+    if (!password || password.length < 6) {
+      validation.isValidPassword = false;
+      check = false;
+      error = "Your Password is invalid";
+    }
+
+    setIsValidInput(validation);
+
+    if (!check && error) {
+      toast.error(error);
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleSubmit = async () => {
+    if (!isValid) {
+      return;
+    }
+
+    let check = isValid();
+
+    let data = { valueLogin, password };
+
+    if (check) {
+      let res = await handleLogin(valueLogin, password);
+      console.log(">>>>>Check res: ", res);
+    }
+  };
 
   return (
     <div className="bg-gray-100 text-gray-900 flex justify-center h-[calc(100vh-4rem)] py-12">
-        <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12 flex border border-black">
-          <img src="https://picsum.photos/200/300" className="mx-auto" />
-        </div>
-        <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12 border border-black flex justify-center">
-          <div className="mt-12 flex flex-col items-center">
-            <h1 className="text-2xl xl:text-3xl font-extrabold">
-                Login
-            </h1>
-                    <div className="w-full flex-1 mt-8">                   
-                        <div className="mx-auto max-w-xs">
+      <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12 flex border border-black">
+        <img src="https://picsum.photos/200/300" className="mx-auto" />
+      </div>
+      <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12 border border-black flex justify-center">
+        <div className="mt-12 flex flex-col items-center">
+          <h1 className="text-2xl xl:text-3xl font-extrabold">Login</h1>
+          <div className="w-full flex-1 mt-8">
+            <div className="mx-auto max-w-xs">
+              <label
+                className="text-gray-600 font-medium "
+                htmlFor="valueLogin"
+              >
+                Email or phone number:
+              </label>
+              <input
+                className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                type="text"
+                placeholder="Email or phone number"
+                name="valueLogin"
+                value={valueLogin}
+                onChange={(e) => setValueLogin(e.target.value)}
+              />
 
-                            <label className="text-gray-600 font-medium " htmlFor="valueLogin">Email or phone number:</label>
-                            <input
-                                className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                                type="text" placeholder="Email or phone number" name="valueLogin" value={valueLogin} onChange={(e) => setValueLogin(e.target.value)}/>
+              <div className="mt-5">
+                <label className="text-gray-600 font-medium" htmlFor="password">
+                  Password:
+                </label>
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white "
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
 
-                            <div className="mt-5">
-                                <label className="text-gray-600 font-medium" htmlFor="password">Password:</label>
-                                <input
-                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white "
-                                    type="password" placeholder="Password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                            </div>
-                        
-                            <button
-                                onClick={() => handleSubmit()}
-                                className="mt-5 tracking-wide font-semibold bg-black text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
-                                >
-                                <svg className="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round">
-                                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                                    <circle cx="8.5" cy="7" r="4" />
-                                    <path d="M20 8v6M23 11h-6" />
-                                </svg>
-                                <span className="ml-3">
-                                    Login
-                                </span>
-                            </button>
+              <button
+                onClick={() => handleSubmit()}
+                className="mt-5 tracking-wide font-semibold bg-black text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+              >
+                <svg
+                  className="w-6 h-6 -ml-2"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                  <circle cx="8.5" cy="7" r="4" />
+                  <path d="M20 8v6M23 11h-6" />
+                </svg>
+                <span className="ml-3">Login</span>
+              </button>
 
-                            <p className="mt-6 text-xs text-gray-600 text-center flex gap-1 justify-center">
-                                Don't have an account?
-                                <Link to="/register" className="border-b border-gray-500 border-dotted text-indigo-500">
-                                    Register
-                                </Link>
-                            </p>
-                        </div>
-                    </div>
-                </div>       
+              <p className="mt-6 text-xs text-gray-600 text-center flex gap-1 justify-center">
+                Don't have an account?
+                <Link
+                  to="/register"
+                  className="border-b border-gray-500 border-dotted text-indigo-500"
+                >
+                  Register
+                </Link>
+              </p>
             </div>
+          </div>
         </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Login;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,12 +1,19 @@
 import axios from "axios";
 
-const registerNewUser = (email, password, phone, username) =>{
-     return axios.post("http://localhost:8080/api/v1/register", {
-                email: email,
-                password: password,
-                phone: phone,
-                username: username,
-            });
-}
+const registerNewUser = (email, password, phone, username) => {
+  return axios.post("http://localhost:8080/api/v1/register", {
+    email: email,
+    password: password,
+    phone: phone,
+    username: username,
+  });
+};
 
-export {registerNewUser};
+const handleLogin = (valueLogin, password) => {
+  return axios.post("http://localhost:8080/api/v1/login", {
+    valueLogin,
+    password,
+  });
+};
+
+export { registerNewUser, handleLogin };
